84d3312b840a2b50504062c08b781cb4b3e80da3
TheEvolutio <x4k.er@yandex.ru>
2019-08-27 18:00:45 +0300

    Commit Evolution

---
+++ EVOLUTION.ru/FullScreenScript.js
@@ -0,0 +1,28 @@
+function viewGame () {
+		var el = document.body;
+		toggleFullscreen(el);
+	};
+
+	function toggleFullscreen (el) {
+	if(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement){
+		if(document.exitFullscreen){
+			document.exitFullscreen();
+		}else if(document.mozCancelFullScreen){
+			document.mozCancelFullScreen();
+		}else if(document.webkitExitFullscreen){
+			document.webkitExitFullscreen();
+		}else if(document.msExitFullscreen){
+			document.msExitFullscreen();
+		}
+	}else{
+		if(document.documentElement.requestFullscreen){
+			el.requestFullscreen();
+		}else if(document.documentElement.mozRequestFullScreen){
+			el.mozRequestFullScreen();
+		}else if(document.documentElement.webkitRequestFullscreen){
+			el.webkitRequestFullscreen();
+		}else if(document.documentElement.msRequestFullscreen){
+			el.msRequestFullscreen();
+		}
+	}
+};
